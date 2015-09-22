a1cb563cf20d61f883462375b9ce32057b417679
App Academy Student <student@appacademy.io>
2015-09-22 14:24:50 -0700

    Finished phase 2

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 gem "webrick", "~> 1.3.1"
 gem "rspec", "~> 3.1.0"
 gem "activesupport"
+gem "better_errors"
+gem "binding_of_caller"
--- lib/phase2/controller_base.rb
+++ lib/phase2/controller_base.rb
@@ -4,20 +4,33 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
+      @already_build_response = nil
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      raise "Redirect Double Render" if already_built_response?
+      #puts @res.header
+      @res.header['location'] = url
+      @res.status = 302
+      @already_built_response = true
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, content_type)
+      raise "Double Render" if already_built_response?
+      @res.body = content
+      @res.content_type = content_type
+      @already_built_response = true
     end
   end
 end
